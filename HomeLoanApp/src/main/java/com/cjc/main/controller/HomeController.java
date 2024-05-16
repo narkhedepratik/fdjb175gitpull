ffaa7bbd9d328e9d0a996d38a5ef693fb78489d3
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2024-05-16 09:04:55 +0530

    dev 2 get api

--- HomeLoanApp/src/main/java/com/cjc/main/controller/HomeController.java
+++ HomeLoanApp/src/main/java/com/cjc/main/controller/HomeController.java
@@ -2,7 +2,8 @@ package com.cjc.main.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HomeController {
 	
@@ -12,5 +13,10 @@ public class HomeController {
 	{
 		 
 		return "Enquiry Saved";
+	}
+	@GetMapping("/view_enquiries");
+	publlic List getAllEnquiries()
+	{
+            return null;
 	}
 }
